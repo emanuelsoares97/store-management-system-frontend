4fffdb73cbdc6aa5f3ba5fb82b003ff28a677216
emanuelsoares97 <tutasoares17@gmail.com>
2025-07-07 17:48:17 +0100

    at

--- main.js
+++
@@ -1,8 +0,0 @@
-import { loadNavbar } from './components/navbar.js'
-import { loadFooter } from './components/footer.js'
-
-document.addEventListener('DOMContentLoaded', () => {
-  loadNavbar('navbar')
-  loadFooter('footer')
-
-})
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    port: 5173,
-  },
-  base: '/', // base para os assets
+  base: '/store-management-system-frontend/',
 })
